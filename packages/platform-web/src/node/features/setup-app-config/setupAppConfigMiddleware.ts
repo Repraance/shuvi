9b4f225a11b53e55fe18220d0ef04e86de74ed56
Repraance <reprance@gmail.com>
2023-12-28 12:20:36 +0800

    feat: add appConfigMiddleware

---
+++ packages/platform-web/src/node/features/setup-app-config/setupAppConfigMiddleware.ts
@@ -0,0 +1,18 @@
+import { IServerPluginContext, ShuviRequestHandler } from '@shuvi/service';
+
+export const getSetupAppConfigMiddleware = (
+  context: IServerPluginContext
+): ShuviRequestHandler => {
+  return async (req, _res, next) => {
+    const appConfig = context.serverPluginRunner.getAppConfig({ req });
+    if (appConfig) {
+      if (typeof appConfig.router.basename !== 'string') {
+        throw new Error(
+          '[ServerPlugin Hook getAppConfig] appConfig.router.basename must be a string'
+        );
+      }
+      context.appConfig = appConfig;
+    }
+    next();
+  };
+};
